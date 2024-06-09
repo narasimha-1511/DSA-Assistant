8634b0d25ca931ed6a88ec29a870cd996d2aa306
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-06-09 09:03:16 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,12 @@ app.get('*', (req, res) => {
   res.sendFile(indexPath);
 });
 
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to the API",
+  });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
